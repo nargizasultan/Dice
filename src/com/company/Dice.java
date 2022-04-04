ca51b31303770efc9b86cb6047ba869714af41eb
nargiza <sultanmuratova500@gmail.com>
2022-04-04 18:57:18 +0600

    c

--- src/com/company/Dice.java
+++ src/com/company/Dice.java
@@ -36,71 +36,71 @@ public class Dice {
        }
 
     }
-    public  void toCheat(int cheatingNumber){
-        switch (cheatingNumber){
-            case 1:
-                Sides.firstSide();
-                score+=number;
-                break;
-            case 2:
-                Sides.secondSide();
-                score+=number;
-                break;
-            case 3:
-                Sides.thirdSide();
-                score+=number;
-                break;
-            case 4:
-                Sides.forthSide();
-                score+=number;
-                break;
-            case 5:
-                Sides.fifthSide();
-                score+=number;
-                break;
-            case 6:
-                Sides.sixthSide();
-                score+=number;
-                break;
-            case 7:
-                Sides.firstSide();
-                System.out.println();
-                Sides.sixthSide();
-                score+=number;
-                break;
-            case 8:
-                Sides.forthSide();
-                System.out.println();
-                Sides.secondSide();
-                score+=number;
-                break;
-            case 9:
-                Sides.thirdSide();
-                System.out.println();
-                Sides.sixthSide();
-                score+=number;
-                break;
-            case 10:
-                Sides.fifthSide();
-                System.out.println();
-                Sides.fifthSide();
-                score+=number;
-                break;
-            case 11:
-                Sides.fifthSide();
-                System.out.println();
-                Sides.sixthSide();
-                score+=number;
-                break;
-            case 12:
-                Sides.sixthSide();
-                System.out.println();
-                Sides.sixthSide();
-                score+=number;
-                break;
-        }
-
-    }
+//    public  void toCheat(int cheatingNumber){
+//        switch (cheatingNumber){
+//            case 1:
+//                Sides.firstSide();
+//                score+=number;
+//                break;
+//            case 2:
+//                Sides.secondSide();
+//                score+=number;
+//                break;
+//            case 3:
+//                Sides.thirdSide();
+//                score+=number;
+//                break;
+//            case 4:
+//                Sides.forthSide();
+//                score+=number;
+//                break;
+//            case 5:
+//                Sides.fifthSide();
+//                score+=number;
+//                break;
+//            case 6:
+//                Sides.sixthSide();
+//                score+=number;
+//                break;
+//            case 7:
+//                Sides.firstSide();
+//                System.out.println();
+//                Sides.sixthSide();
+//                score+=number;
+//                break;
+//            case 8:
+//                Sides.forthSide();
+//                System.out.println();
+//                Sides.secondSide();
+//                score+=number;
+//                break;
+//            case 9:
+//                Sides.thirdSide();
+//                System.out.println();
+//                Sides.sixthSide();
+//                score+=number;
+//                break;
+//            case 10:
+//                Sides.fifthSide();
+//                System.out.println();
+//                Sides.fifthSide();
+//                score+=number;
+//                break;
+//            case 11:
+//                Sides.fifthSide();
+//                System.out.println();
+//                Sides.sixthSide();
+//                score+=number;
+//                break;
+//            case 12:
+//                Sides.sixthSide();
+//                System.out.println();
+//                Sides.sixthSide();
+//                score+=number;
+//                break;
+//        }
+//
+//    }
 
     public int getPenalty() {
         return penalty;
